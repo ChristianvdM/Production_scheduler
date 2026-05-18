e27a4e29002979f891cfa91d6be529e2cf645b0e
Christian van der Merwe <46787925+ChristianvdM@users.noreply.github.com>
2026-05-18 22:01:45 +0200

    Update scoring.py

--- scheduler/scoring.py
+++ scheduler/scoring.py
@@ -1,15 +1,58 @@
 import numpy as np
+
+FAIRNESS_WEIGHT = 5
+PROFICIENCY_WEIGHT = 2
+COVERAGE_WEIGHT = 3
+FATIGUE_WEIGHT = 4
+
+
+class CandidateScorer:
+
+    def __init__(
+        self,
+        assignments_count,
+        campus_assignments,
+        recent_assignments,
+        target_assignments,
+        skills_matrix
+    ):
+
+        self.assignments_count = assignments_count
+
+        self.campus_assignments = campus_assignments
+
+        self.recent_assignments = recent_assignments
+
+        self.target_assignments = target_assignments
+
+        self.skills_matrix = skills_matrix
+
+    def fairness_score(
+        self,
+        person
+    ):
+
         current = self.assignments_count[person]
 
         delta = current - self.target_assignments
 
-        return max(0, 100 - (delta * 15))
+        return max(
+            0,
+            100 - (delta * 15)
+        )
 
-    def proficiency_score(self, skill):
+    def proficiency_score(
+        self,
+        skill
+    ):
 
         return skill * 25
 
-    def campus_balance_score(self, person, campus):
+    def campus_balance_score(
+        self,
+        person,
+        campus
+    ):
 
         counts = self.campus_assignments[person]
 
@@ -20,18 +63,35 @@ import numpy as np
 
         avg = np.mean(values)
 
-        return max(0, 25 - abs(counts[campus] - avg) * 5)
+        return max(
+            0,
+            25 - abs(counts[campus] - avg) * 5
+        )
 
-    def fatigue_penalty(self, person):
+    def fatigue_penalty(
+        self,
+        person
+    ):
 
-        return self.recent_assignments[person] * 20
+        return (
+            self.recent_assignments[person] * 20
+        )
 
-    def elite_penalty(self, person):
+    def elite_penalty(
+        self,
+        person
+    ):
 
-        avg_skill = self.skills_matrix.loc[person].mean()
+        avg_skill = (
+            self.skills_matrix.loc[person]
+            .mean()
+        )
 
         if avg_skill >= 2.5:
-            return self.assignments_count[person] * 12
+
+            return (
+                self.assignments_count[person] * 12
+            )
 
         return 0
 
@@ -56,9 +116,9 @@ import numpy as np
         elite = self.elite_penalty(person)
 
         return (
-            FAIRNESS_WEIGHT * fairness +
-            PROFICIENCY_WEIGHT * proficiency +
-            COVERAGE_WEIGHT * campus_balance -
-            FATIGUE_WEIGHT * fatigue -
-            elite
+            FAIRNESS_WEIGHT * fairness
+            + PROFICIENCY_WEIGHT * proficiency
+            + COVERAGE_WEIGHT * campus_balance
+            - FATIGUE_WEIGHT * fatigue
+            - elite
         )
